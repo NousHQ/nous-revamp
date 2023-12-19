1a29183358ca8e44b9f6b0ee6076f908f819d77b
Siddharth Balyan <balyan.sid@gmail.com>
2023-12-19 09:40:45 +0530

    Refactor SendAuthExtension component

--- app/send-auth-extension.tsx
+++ app/send-auth-extension.tsx
@@ -1,27 +1,35 @@
 "use client"
 
-import { useEffect } from "react";
-import { useSearchParams, useRouter } from 'next/navigation';
+import { useEffect } from "react"
+import { useRouter, useSearchParams } from "next/navigation"
 
-export default function SendAuthExtension({access_token}: {access_token: string}) {
-  const searchParams = useSearchParams();
-  const extExists = searchParams.has('ext');
+export default function SendAuthExtension({
+  access_token,
+}: {
+  access_token: string
+}) {
+  const searchParams = useSearchParams()
+  const extExists = searchParams.has("ext")
   const router = useRouter()
   useEffect(() => {
-    if (extExists && chrome.runtime !== undefined) {
-      const extensionId = process.env.NEXT_PUBLIC_EXTENSION_ID;
-      chrome.runtime.sendMessage(
-        extensionId,
-        { action: "getJWT", access_token: access_token },
-        (response: any) => {
-          if (!response) {
-            console.log("no extension");
-          } else {
-            console.log(response);
+    try {
+      if (extExists && chrome.runtime !== undefined) {
+        const extensionId = process.env.NEXT_PUBLIC_EXTENSION_ID
+        chrome.runtime.sendMessage(
+          extensionId,
+          { action: "getJWT", access_token: access_token },
+          (response: any) => {
+            if (!response) {
+              console.log("no extension")
+            } else {
+              console.log(response)
+            }
           }
-        }
-      );
-      router.replace(`/`);
-    } 
-  }, [access_token, extExists]);
+        )
+        router.replace(`/`)
+      }
+    } catch (error) {
+      console.error("An error occurred:", error)
+    }
+  }, [access_token, extExists])
 }
